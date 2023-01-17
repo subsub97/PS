e5adbc036573d2df5caf7cd2fed8c8227b8f2e58
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-01-17 19:17:46 +0900

    [Silver V] Title: 소수 찾기, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,13 +1,18 @@
-import sys
+import sys,math
 n = sys.stdin.readline().strip()
 num_list = list(map(int,sys.stdin.readline().split()))
 
 total_cnt = 0
 for num in num_list:
     cnt = 0
-    for i in range(1,num+1):
+    s = math.sqrt(num)
+    for i in range(1,int(math.sqrt(num) +1)):
+
         if num % i == 0:
             cnt += 1
-    if cnt == 2:
-        total_cnt += 1
+            if cnt > 2:
+                break
+    if cnt == 1:
+        if num != 1:
+            total_cnt += 1
 print(total_cnt)
