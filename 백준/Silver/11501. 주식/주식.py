ac00de0867ec58c215339a7dd8647c07c86c1f85
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-06-03 23:39:38 +0900

    [Silver II] Title: 주식, Time: 1496 ms, Memory: 336736 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,9 +1,10 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 for _ in range(n):
     max_profit = 0
     balance = 0
-    k = int(input())
-    stock_prices = list(map(int,input().split()))
+    k = int(sys.stdin.readline())
+    stock_prices = list(map(int,sys.stdin.readline().split()))
     for i in range(k-1,-1,-1):
         if (stock_prices[i] > max_profit):
             max_profit = stock_prices[i]
