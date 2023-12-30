c03fd3e6c83fdce44c082d3d7552b10cc5c726f4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-12-30 17:20:33 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 123 ms, Memory: 23,576 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,22 +1,18 @@
-
-
 import java.io.*;
 import java.util.*;
 
 public class Solution{
     static int n;
-    static int[][] buildingMap;
+    static int[] buildingHeights;
 
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = 10;
 
+        int T = 10;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int t = 1; t <= T; t++) {
             n = Integer.parseInt(br.readLine().trim());
             int answer = 0;
-            int[] buildingHeights = new int[n];
-            buildingMap = new int[256][n];
-
+            buildingHeights = new int[n];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -24,47 +20,14 @@ public class Solution{
                 buildingHeights[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < buildingHeights[i]; j++) {
-                    buildingMap[j][i] = 1;
-                }
-            }
-
-            for (int i = 0; i < 256; i++) {
-                for (int j = 0; j < n; j++) {
-                    if(checkView(i,j)) {
-                        answer++;
-                    }
+            for (int i = 2; i < n - 2; i++) {
+                int currentHeight = buildingHeights[i];
+                int maxNeighborHeight = Math.max(Math.max(buildingHeights[i - 2], buildingHeights[i - 1]), Math.max(buildingHeights[i + 1], buildingHeights[i + 2]));
+                if(currentHeight > maxNeighborHeight) {
+                    answer += currentHeight - maxNeighborHeight;
                 }
             }
             System.out.println("#" + t +" "+answer);
         }
     }
-
-    static boolean checkView(int row, int col) {
-        if(buildingMap[row][col] == 0) {
-            return false;
-        }
-        for (int i = 1; i <= 2; i++) {
-            int checkPreCol = col - i;
-            int checkAferCol = col + i;
-
-            if(!inRange(row,checkAferCol)) return false;
-            if(!inRange(row,checkPreCol)) return false;
-
-            if(buildingMap[row][checkAferCol] == 1 || buildingMap[row][checkPreCol] == 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean inRange(int row,int col) {
-        return row >= 0 && row < 256 && col >= 0 && col < n;
-    }
 }
-
-
-
-
-
