5d2c50a25929a6e58a042413b3b69c7e3334970e
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-04-11 17:15:13 +0900

    [Silver III] Title: 두 수의 합, Time: 408 ms, Memory: 27140 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,36 +1,47 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static int n,x;
+    public static int[] numbers;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+
+        n = Integer.parseInt(br.readLine());
+        numbers = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(br.readLine());
+        x = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        int start=0,end=n-1;
-        int result = 0;
-        int cur_sum;
-        while(start < end){
-            cur_sum = arr[start] + arr[end];
-            if(cur_sum == x){
-                result++;
-                end--;
+        Arrays.sort(numbers);
+
+        int leftIdx = 0;
+        int rightIdx = n-1;
+        int answer = 0;
+
+        while(leftIdx < rightIdx) {
+            int curSum = numbers[leftIdx] + numbers[rightIdx];
+
+            if(curSum == x) {
+                answer++;
+            }
+
+            if(curSum > x) {
+                rightIdx--;
             }
             else{
-                // 현재 합이 x값보다 큰경우
-                if(cur_sum > x){
-                    end--;
-                }
-                //cur_sum이 x보다 작은경우
-                else start++;
+                leftIdx++;
             }
         }
-        System.out.println(result);
+
+        System.out.println(answer);
+
     }
 }
