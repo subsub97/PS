1559b737c882dbf1670cdf961a51e67d50e424c1
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-04-19 11:23:41 +0900

    [level 2] Title: 소수 찾기, Time: 3.12 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    ArrayList<Integer> num = new ArrayList<>();
+    boolean[] check = new boolean[10000000];
+    boolean[] selected;
+        
+    public int solution(String numbers) {
+    
+        selected = new boolean[numbers.length()];
+        int[] number = new int[numbers.length()];
+            
+        for(int i = 0; i < numbers.length(); i++) {
+            number[i] = Integer.parseInt(numbers.substring(i,i+1));
+        }
+        
+        choose(0, number);
+        
+        return answer;
+    }
+    
+    public void choose(int depth, int[] number) {
+        if(isPrime()) {
+            for(int i = 0; i < num.size(); i++) {
+                System.out.print(num.get(i));
+                
+            }
+            System.out.println();
+            answer++;
+        }
+            
+        if(depth == number.length) {
+            return;
+        }
+        
+        for(int i = 0; i < number.length; i++) {
+            if(selected[i]) continue;
+            if(num.size() == 0 && number[i] == 0) continue;
+            
+            selected[i] = true;
+            num.add(number[i]);
+            choose(depth+1, number);
+            num.remove(num.size() - 1);
+            selected[i] = false;
+            
+        }
+    }
+    
+    public boolean isPrime() {
+        if(num.size() == 0) return false;
+        
+        String tempNum = "";
+            
+        for(int i = 0; i < num.size(); i++) {
+            tempNum += num.get(i);
+        }
+        
+        int number = Integer.parseInt(tempNum);
+        if(number == 1) return false;
+        if(check[number]) return false;
+        check[number] = true;
+        
+        for(int i = 2; i <= Math.sqrt(number); i++) {
+            if(number % i  == 0) return false;
+        }
+        return true;
+    }
+    
+    
+}
