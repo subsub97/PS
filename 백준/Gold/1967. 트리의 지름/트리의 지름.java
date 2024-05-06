88c2c06175e3314522cf44c59e726778b374422b
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-05-06 16:09:22 +0900

    [Gold IV] Title: 트리의 지름, Time: 2408 ms, Memory: 121048 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n,maxCost;
+    static ArrayList<Node>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b, cost));
+            graph[b].add(new Node(a, cost));
+        }
+
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
+            dfs(i, 0);
+        }
+
+        System.out.println(maxCost);
+    }
+
+    public static void dfs(int v,int costSum) {
+        visited[v] = true;
+
+        if (costSum > maxCost) {
+            maxCost = costSum;
+        }
+
+        for (int i = 0; i < graph[v].size(); i++) {
+            Node nextNode = graph[v].get(i);
+            if(visited[nextNode.vertex]) continue;
+
+            dfs(nextNode.vertex, costSum + nextNode.cost);
+        }
+    }
+
+    static class Node{
+        int vertex;
+        int cost;
+
+        Node(int v, int c) {
+            this.vertex = v;
+            this.cost = c;
+        }
+    }
+}
